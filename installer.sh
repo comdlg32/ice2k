e38ed81f2ef4da613a916a4d7fb7122d9201ad5a
0penrc <80153347+0penrc@users.noreply.github.com>
2025-10-01 01:31:43 +0200

    m

--- installer.sh
+++ installer.sh
@@ -10,13 +10,43 @@ libsm-dev libice-dev libxrandr-dev libxft-dev libxpm-dev libjpeg-dev \
 libpng-dev libfontconfig1-dev libxinerama-dev libfribidi-dev libimlib2-dev xterm \
 x11-utils xinit intltool intltool-debian libxcb-util-dev libx11-xcb-dev \
 build-essential libfox-1.6-dev psmisc libxtst-dev libserialport-dev \
-libpci-dev feh \
+libpci-dev feh tk imagemagick \
 || exit
 
 
 if [ "$ONLYTOOLS" -eq 1 ]; then
 	exit
 fi
+
+
+simplebuild()
+{
+	(
+	set -e
+	cd "$1"
+	make clean
+	make -j$(nproc)
+	sudo make install
+	set +e
+	) || exit
+
+}
+
+
+
+simplebuild i2klibs/ini
+simplebuild i2klibs/branding
+simplebuild i2klibs/comctl32
+simplebuild games/winmine
+simplebuild mmc/devmgmt
+simplebuild ice2kver
+simplebuild batmeter
+simplebuild shutdown
+simplebuild msgina
+simplebuild shell/hotplug
+simplebuild shell/backmgr
+simplebuild control/sysdm
+simplebuild control/desk
 
 (
 cd icewm-3.3.1
@@ -25,7 +55,7 @@ aclocal
 autoheader
 autoupdate
 automake --add-missing --force-missing --copy --foreign
-autoreconf
+autoreconf -vif
 set -e
 ./configure
 make clean
@@ -50,29 +80,47 @@ sudo make install
 set +e
 ) || exit
 
-simplebuild()
-{
-	(
-	set -e
-	cd "$1"
-	make clean
-	make -j$(nproc)
-	sudo make install
-	set +e
-	) || exit
+(
+cd yad-*
 
-}
+aclocal
+autoheader
+autoupdate
+automake --add-missing --force-missing --copy --foreign
+autoreconf
+set -e
+./configure
+make clean
+make -j$(nproc)
+sudo make install
+set +e
+) || exit
 
-simplebuildõi2klibs/ini
-simplebuildõi2klibs/branding
-simplebuildõi2klibs/comctl32
-simplebuildõgames/winmine
-simplebuildõmmc/devmgmt
-simplebuildõice2kver
-simplebuildõbatmeter
-simplebuildõshutdown
-simplebuildõmsgina
-simplebuildõshell/hotplug
-simplebuildõshell/backmgr
-simplebuildõcontrol/sysdm
-simplebuild control/desk
+(
+cd cbatticon
+
+make clean
+make
+sudo make install
+) || exit
+
+
+
+mv ~/.icewm ~/.icewm-old
+cp -rf icewm-dir ~/.icewm
+cp -rf .config ~/
+cp -rf .foxrc ~/
+cp -rf .idesktop ~/
+cp -rf .icons ~/
+cp -rf .themes ~/
+
+sudo cp winvga-12.pcf /usr/share/fonts/X11/misc
+
+# from wine
+sudo cp tahoma*.ttf /usr/share/fonts/truetype
+sudo cp tahoma*.pcf.gz /usr/share/fonts/X11/100dpi
+
+sudo update-fonts-dir misc
+sudo update-fonts-alias misc
+sudo update-fonts-dir 100dpi
+sudo update-fonts-alias 100dpi
